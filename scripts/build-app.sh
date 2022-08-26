02081af7f346659175c0c7c379f4098eb80a8532
GianlucaMac <gianlucaberarducci@gmail.com>
2022-08-26 14:56:14 -0300

    anda

--- scripts/build-app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t web:v1 teleco/client
-docker run -ti --name web -p 8080:80 web:v1
--- scripts/build-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-#docker build -f dockerfiles/Dockerfile.server -t teleco:$1 server
-docker build -t teleco:$1 server
-
-# docker run -ti -p 8080:8080 --name teleco teleco:v1
